bb23c4fff245607583ac9cc69709e3154ed315fb
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-11 21:28:22 -0600

    More Auth Stuff

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,8 @@ import Auth from '../components/Auth/Auth';
 import './App.scss';
 import connection from '../helpers/data/connection';
 import Listings from '../components/Listings/Listings';
+import MyNavbar from '../components/MyNavBar/MyNavbar';
+import authRequests from '../helpers/data/authRequests';
 
 
 class App extends Component {
@@ -19,15 +21,22 @@ class App extends Component {
   }
 
   render() {
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
     if (!this.state.authed) {
       return (
         <div className="App">
+          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
           <Auth isAuthenticated={this.isAuthenticated} />
         </div>
       );
     }
     return (
       <div className="App">
+        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
         <Listings />
       </div>
     );
