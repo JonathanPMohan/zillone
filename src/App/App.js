a99686e3af92134fc1bef95d15297b46a675f02a
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-11 20:51:36 -0600

    More State Stuff

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import Auth from '../components/Auth/Auth';
 import './App.scss';
 import connection from '../helpers/data/connection';
+import Listings from '../components/Listings/Listings';
 
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <Auth />
+        <Listings />
       </div>
     );
   }
