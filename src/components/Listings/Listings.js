d205ac3363607d6e0ae031728a99d0006013817c
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-15 11:20:05 -0600

    updated some listing styles

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import listingShape from '../../helpers/props/listingShape';
+import ListingItem from '../ListingItem/ListingItem';
 import './Listings.scss';
 
 
 class Listings extends React.Component {
+  static PropTypes = {
+    listings: PropTypes.arrayOf(listingShape),
+  }
+
   render() {
+    const { listings } = this.props;
+    const listingsItemComponents = listings.map(listing => (
+      <ListingItem key={listing.id} listing={listing} />
+    ));
     return (
       <div className="listings col">
         <h2>Listings</h2>
+        {listingsItemComponents}
       </div>
     );
   }
